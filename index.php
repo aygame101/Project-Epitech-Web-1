4c90a8933017283dbbc4af6d69edbe46d3dff835
aygame101 <aygame101@gmail.com>
2024-10-14 14:22:28 +0200

    change index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 
 <head>
-    <link rel="stylesheet" href="style_index.css">
+    <link rel="stylesheet" href="css/style_index.css">
     <script src="js/script.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
