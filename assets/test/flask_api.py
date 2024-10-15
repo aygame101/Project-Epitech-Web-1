2b93ddda3be9fed0b30699774243713cf208792a
Ryan <ryan.wittert@outlook.fr>
2024-10-15 12:10:14 +0200

    Update flask_api.py

--- assets/test/flask_api.py
+++ assets/test/flask_api.py
@@ -1,53 +1,48 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://username:@localhost/web_project"  # a remplacer avec nos base de donnée
-db = SQLAlchemy(app)
+# change string to the name of your database; add path if necessary
+db_name = 'web_projet.sql'
 
-# personnes
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
 
-class People(db.Model):
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# initialize the app with Flask-SQLAlchemy
+db.init_app(app)
+
+
+# NOTHING BELOW THIS LINE NEEDS TO CHANGE
+# this route will test the database connection - and nothing more
+@app.route('/')
+def testdb():
+    try:
+        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+        return '<h1>It works.</h1>'
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# a modif selon la database
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    applying = db.Column(db.Integer, nullable=False)
-    mail = db.Column(db.String(255), nullable=False)
-    name = db.Column(db.String(100))
-    firstname = db.Column(db.String(100))
-    phone = db.Column(db.String(20))
-    password = db.Column(db.String(255))
-    salt = db.Column(db.String(255))
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
 
-# companies
-
-class Companies(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False, unique=True)
-    username = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    salt = db.Column(db.String(255), nullable=False)
-    
-# Ad
-
-class Advertising(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    company_name = db.Column(db.String(100), db.ForeignKey('companies.name'))
-    poste = db.Column(db.String(255), nullable=False)
-    lieu = db.Column(db.String(255), nullable=False)
-    salaire = db.Column(db.String(20), nullable=False)
-    contrat = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    
-# storage
-
-class RequestStorage(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    id_annonce_postule = db.Column(db.Integer, db.ForeignKey('advertising.id'))
-    applayer_info = db.Column(db.Integer, db.ForeignKey('people.id'))
-    id_company = db.Column(db.Integer, db.ForeignKey('companies.id'))
-
+# a repéter avec les differents profils et par conséquent à adapter (postulant[creer, lire] , entreprises[créer, lire, modifier] et admin[créer, lire, modifier, supprimer])
 
                             #======================ADMIN======================
-                            
 @app.route("/users", methods=["GET"])
 def get_users():
     users = User.query.all()
@@ -92,7 +87,7 @@ def delete_user(id):
 if __name__ == "__main__":
     app.run(debug=True)
     
-                         #======================Applyer======================
+                      #======================Applyer======================
                          
 @app.route("/users", methods=["GET"])
 def get_users():
@@ -126,7 +121,7 @@ def update_user(id):
     db.session.commit()
     return jsonify({"message": "User  updated successfully"})
 
-                    #======================Company======================
+                    #======================Applyer======================
                     
 @app.route("/users", methods=["GET"])
 def get_users():
@@ -159,3 +154,8 @@ def update_user(id):
     user.email = data["email"]
     db.session.commit()
     return jsonify({"message": "User  updated successfully"})
+
+
+#======================TEST PART======================
+
+#import pytest
