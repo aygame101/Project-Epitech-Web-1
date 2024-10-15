fdb4dfc8df09219631def2c7207f64abb9f9702c
aygame101 <aygame101@gmail.com>
2024-10-15 11:54:23 +0200

    form to add job add working

--- pages/post_job_ad.php
+++ pages/post_job_ad.php
@@ -2,32 +2,43 @@
 
 $company_name = nl2br(htmlspecialchars($_POST['company_name']));
 $city = nl2br(htmlspecialchars($_POST['city']));
-$job = nl2br(htmlspecialchars($_POST['job']));
-$contract = nl2br(htmlspecialchars($_POST['contract']));
+$job_title = nl2br(htmlspecialchars($_POST['job']));
+$contract_type = nl2br(htmlspecialchars($_POST['contract']));
 $wage = nl2br(htmlspecialchars($_POST['wage']));
 $mail_in_charge = nl2br(htmlspecialchars($_POST['in_charge']));
-$job_description = nl2br(htmlspecialchars($_POST['job_description']));
+$description_job = nl2br(htmlspecialchars($_POST['description_job']));
+
 
 require_once('../assets/hypnos.php');
 
-try
-{
+try {
+    
     $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-} catch (PDOException $e)
-{
-    print "Erreur :" . $e->getMessage() . "<br/>";
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    
+    $req = $bdd->prepare('
+        INSERT INTO job_ads(company_name, city, job_title, contract_type, wage, mail_in_charge, description_job) 
+        VALUES(:company_name, :city, :job_title, :contract_type, :wage, :mail_in_charge, :description_job)
+    ');
+
+   
+    $req->execute(array(
+        'company_name' => $company_name,
+        'city' => $city,
+        'job_title' => $job_title,
+        'contract_type' => $contract_type,
+        'wage' => $wage,
+        'mail_in_charge' => $mail_in_charge,
+        'description_job' => $description_job
+    ));
+
+    
+    header('Location: form_company.html');
+    exit();
+
+} catch (PDOException $e) {
+    
+    echo "Erreur : " . $e->getMessage();
     die;
 }
-
-$req = $bdd->prepare('INSERT INTO job_ads(company_name, city, job_title, contract_type, wage, mail_in_charge, description) VALUES(:company_name, :city, :job_title, :contract_type, :wage, :mail_in_charge, :description)');
-$req->execute(array(
-    'company_name' => $company_name,
-    'city' => $city,
-    'job_title' => $job,
-    'contract_type' => $contract,
-    'wage' => $wage,
-    'mail_in_charge' => $mail_in_charge,
-    'description' => $job_description
-));
-
-header('Location: form_company.html');
