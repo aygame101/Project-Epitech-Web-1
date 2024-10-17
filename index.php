e2273221acb45261dae65688b243c6b6c9654d4e
aygame101 <aygame101@gmail.com>
2024-10-17 11:05:00 +0200

    contract type done

--- index.php
+++ index.php
@@ -39,6 +39,30 @@
             <select id="contract_type" name="contract_type">
                 <option selected="true" disabled="true">Contract Type</option>
 
+                <?php
+                require_once('assets/hypnos.php');
+
+                // Create connection
+                $conn = mysqli_connect($host, $username, $password, $dbname);
+                // Check connection
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+
+                $sql = "SELECT DISTINCT contract_type FROM job_ads;";
+                $result = mysqli_query($conn, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                    // output data of each row
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo '<option id="'.$row["contract_type"].'" value="'.$row["contract_type"].'">'. ucfirst($row["contract_type"]).'</option>';
+                    }
+                } else {
+                    echo "0 results";
+                }
+
+                mysqli_close($conn);
+                ?>
                 <!-- recup les données dans la BDD -->
             </select>
 
