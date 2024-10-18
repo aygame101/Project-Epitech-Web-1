794d3a35e7acb31029f11b0abc4206b029c90821
Ryan <ryan.wittert@outlook.fr>
2024-10-18 17:03:33 +0200

    Add files via upload

---
+++ pages/logout.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+// Détruire toutes les variables de session
+session_unset();
+
+// Détruire la session
+session_destroy();
+
+// Rediriger vers la page de connexion ou toute autre page souhaitée
+header("Location: connexion.php");
+exit();
+
+
+
+ //bouton poru se déco
+// <form action="logout.php" method="post">
+//<button type="submit">logout</button>
+//</form>
